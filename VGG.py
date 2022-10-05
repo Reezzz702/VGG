76c901db6ae780c171260cc34da78fac93d99179
YuHo-chang <changhoward.cs07@nctu.edu.tw>
2022-10-05 16:55:26 +0800

    add generate test.csv

--- VGG.py
+++ VGG.py
@@ -10,110 +10,6 @@ import os
 import matplotlib.pyplot as plt
 
 
-# class VGG(nn.Module):
-#     def __init__(self, num_classes=10):
-#         super(VGG, self).__init__()
-#         self.layer1 = nn.Sequential(
-#             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU())
-#         self.layer2 = nn.Sequential(
-#             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(), 
-#             nn.MaxPool2d(kernel_size = 2, stride = 2))
-#         self.layer3 = nn.Sequential(
-#             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU())
-#         self.layer4 = nn.Sequential(
-#             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size = 2, stride = 2))
-#         self.layer5 = nn.Sequential(
-#             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU())
-#         self.layer6 = nn.Sequential(
-#             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU())
-#         self.layer7 = nn.Sequential(
-#             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU())
-#         self.layer8 = nn.Sequential(
-#             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size = 2, stride = 2))
-#         self.layer9 = nn.Sequential(
-#             nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU())
-#         self.layer10 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU())
-#         self.layer11 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU())
-#         self.layer12 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size = 2, stride = 2))
-#         self.layer13 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU())
-#         self.layer14 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU())
-#         self.layer15 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU())
-#         self.layer16 = nn.Sequential(
-#             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-#             nn.BatchNorm2d(512),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size = 2, stride = 2))
-#         self.fc = nn.Sequential(
-#             nn.Dropout(0.5),
-#             nn.Linear(7*7*512, 4096),
-#             nn.ReLU())
-#         self.fc1 = nn.Sequential(
-#             nn.Dropout(0.5),
-#             nn.Linear(4096, 4096),
-#             nn.ReLU())
-#         self.fc2= nn.Sequential(
-#             nn.Linear(4096, num_classes))
-        
-#     def forward(self, x):
-#         out = self.layer1(x)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
-#         out = self.layer5(out)
-#         out = self.layer6(out)
-#         out = self.layer7(out)
-#         out = self.layer8(out)
-#         out = self.layer9(out)
-#         out = self.layer10(out)
-#         out = self.layer11(out)
-#         out = self.layer12(out)
-#         out = self.layer13(out)
-#         out = out.reshape(out.size(0), -1)
-#         out = self.fc(out)
-#         out = self.fc1(out)
-#         out = self.fc2(out)
-#         return out
-
-
 VGG19 = [
         64,
         64,
@@ -230,7 +126,7 @@ class SportLoader(Dataset):
         img = io.imread(img_path)
         img = self.transform(img)
         if self.mode == "test":
-            return img
+            return img, img_path
         label = torch.tensor(int(self.label_list[index]))
         return img, label 
 
@@ -282,7 +178,7 @@ test_dataset = SportLoader("test", test_img)
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64)
 
 num_epochs = 50
 learning_rate = 0.005
@@ -351,19 +247,24 @@ plot(num_epochs, train_acc, val_acc, "Accuracy Curve")
 # get # of parameters
 print(num_of_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad))
 
+test_img = []
+test_predicted = []
+with torch.no_grad():
+    for images, img_path in test_loader:
+        images = images.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        test_predicted.extend(predicted.numpy())
+        test_img.extend(img_path)
+        del images, outputs
 
-# with torch.no_grad():
-#     correct = 0
-#     total = 0
-#     for images, labels in test_loader:
-#         images = images.to(device)
-#         labels = labels.to(device)
-#         outputs = model(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-#         del images, labels, outputs
+    print(f'Accuracy of the network on the {total} test images: {100 * correct / total} %')
 
-#     print(f'Accuracy of the network on the {total} test images: {100 * correct / total} %')
+with open("test.csv", 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["names", "label"])
+    for i in range(len(test_img)):
+        writer.writerow([test_img[i], test_predicted[i]])
+    f.close()
 # ref1: https://jaketae.github.io/study/pytorch-vgg/
 # ref2: https://blog.paperspace.com/vgg-from-scratch-pytorch/
